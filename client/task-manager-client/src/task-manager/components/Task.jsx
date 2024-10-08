7a85b66e6e516b91f097b241c267c70c68ae6440
devSummon99 <michelperezsomoza@gmail.com>
2024-10-07 23:50:52 -0400

    estructura-tareas

--- client/task-manager-client/src/task-manager/components/Task.jsx
+++ client/task-manager-client/src/task-manager/components/Task.jsx
@@ -8,7 +8,23 @@ import './task.css'
 function Task() {
   return (
     <>
-     
+     <header>
+      <nav></nav>
+     </header>
+
+     <section>
+      <table></table>
+     </section>
+
+     <section>
+      <div>
+
+      </div>
+
+      <div>
+        
+      </div>
+     </section>
     </>
   )
 }
