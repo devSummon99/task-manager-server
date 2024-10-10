9b4177339208ff93ec049838397ab4b7e53c2782
devSummon99 <michelperezsomoza@gmail.com>
2024-10-09 23:39:10 -0400

    close-open-menu-navbar

--- client/task-manager-client/src/task-manager/components/Task.jsx
+++ client/task-manager-client/src/task-manager/components/Task.jsx
@@ -1,17 +1,28 @@
 //libs
-//import { useEffect } from 'react';
+import { useState } from "react";
 //components
 
 //styles
 import "./task.css";
 
 const Task = () => {
+  const [active, setActive] = useState("nav_menu");
+  const [toggleIcon, setToggleIcon] = useState("nav_toggler")
+
+  const handleNavToggle = () => {
+    active === "nav_menu"
+      ? setActive("nav_menu nav_active")
+      : setActive("nav_menu");
+
+      toggleIcon === "nav_toggler" ? setToggleIcon("nav_toggler toggle") : setToggleIcon("nav_toggler")
+  };
+
   return (
     <nav className="nav">
-      <a href="#" className="task">
+      <a href="#" className="link-task">
         Tasks
       </a>
-      <ul className="nav_menu">
+      <ul className={active}>
         <li className="mav_item">
           <a href="#" className="nav_link">
             All Tasks
@@ -38,10 +49,12 @@ const Task = () => {
           </a>
         </li>
       </ul>
-      <div className="nav_toggler">
-        <div className="line1"></div>
+      <div onClick={handleNavToggle} className={toggleIcon}>
+     <div className="container-lineal">
+     <div className="line1"></div>
         <div className="line2"></div>
         <div className="line3"></div>
+     </div>
       </div>
     </nav>
   );
