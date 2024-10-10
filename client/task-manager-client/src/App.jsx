7e07d3ffac5667add54f0f098882581521a69872
devSummon99 <michelperezsomoza@gmail.com>
2024-10-09 23:58:55 -0400

    react-router-doom-installed

--- client/task-manager-client/src/App.jsx
+++ client/task-manager-client/src/App.jsx
@@ -1,26 +1,14 @@
-//libs
-import { } from 'react';
-//components
-//import Auth from './session-manager/auth/components/Auth';
-//import PasswordRecovery from "./session-manager/password-recovery/components/PasswordRecovery";
-//import ChangePassword from "./session-manager/password-recovery/components/ChangePassword";
-//styles
-import './App.css';
-import Task from './task-manager/components/Task';
+import "./App.css";
+import Task from "./task-manager/components/Task";
 
 
 function App() {
-
   return (
     <>
-     <Task/>
-    </>
-  )
-  /*
- < PasswordRecovery/>
-   <Auth/>
-    <ChangePassword/>
-  */ 
-}
+  
+        <Task />
 
+  </>
+  )
+}
 export default App;
