24b67dbf3f64906a6bc8952058d275949ae551dc
devSummon99 <michelperezsomoza@gmail.com>
2024-11-11 16:09:25 -0500

    security-db-config

--- server/task-manager-server/src/db/config.js
+++ server/task-manager-server/src/db/config.js
@@ -1,9 +1,9 @@
 import pg from "pg";
 
 export const connect = new pg.Pool ( {
-    user:"admin",
-    host: "localhost",
-    port: "5433",
-    password: "admin",
-    database:"tasks"
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    password: process.env.DB_PASSWORD,
+    database:process.env.DB_NAME
 })
